b40ea5d4e1ab6c45095eb668f12bbde089fa0157
thewhitemage13 <144810207+thewhitemage13@users.noreply.github.com>
2024-02-11 20:37:34 +0100

    Update BOMBERMAN.cpp

--- BOMBERMAN/BOMBERMAN/BOMBERMAN.cpp
+++ BOMBERMAN/BOMBERMAN/BOMBERMAN.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <ctime>
 #include <Windows.h>
 using namespace std;
@@ -9,27 +9,36 @@ public:
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
     int width;
     int height;
-    int** bomber = new int* [width];
+    int** bomber = nullptr;
 
-    BombermanGame(int w, int h) : width(w), height(h) {
-
-        bomber = new int* [width];
-        for (int i = 0; i < width; ++i) {
-            bomber[i] = new int[height];
+    BombermanGame(int w, int h) : width(w), height(h) 
+    {
+        bomber = new int* [height];
+        for (int i = 0; i < height; ++i) {
+            bomber[i] = new int[width];
         }
     }
-
+    //очистка курсора
+    void Options()
+    {
+        HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+        // размер окна - 130х50 символов
+        CONSOLE_CURSOR_INFO ci; // скрытие курсора
+        ci.bVisible = false; // курсор не видно
+        ci.dwSize = 100; // на самом деле, не важно что сюда писать - подходит любое значение от 1 до 100 (рамер курсора в процентах относительно клетки)
+        SetConsoleCursorInfo(h, &ci); // применить настройки
+    }
+    //очистка памяти
     ~BombermanGame() 
     {
         FreeMemory();
     }
-
+    //основные стены
     void Wall()
     {
         for (int y = 0; y < height; y++)
         {
-            bomber[y] = new int[height];
-
+           
             for (int x = 0; x < width; x++)
             {
                 bomber[y][x] = rand() % 5;
@@ -38,41 +47,52 @@ public:
                 {
                     bomber[y][x] = 1;
                 }
-                if (x == 0 || y == 0 || x == width - 1 || y == height - 1 || x == 1 || x == 2 || y == 1 || x == width - 2 || y == height - 2 || x == width - 3)//стена
+                if (x == 0 || y == 0 || x == width - 1 || y == height - 1 || x == 1 || x == 2 || y == 1 || x == width - 2 || y == height - 2 || x == width - 3)
                 {
-                    SetCursor(x, y, 8, "");
+                    SetCursor(x, y, 8);
                     cout << (char)178;
                 }
-
-
             }
         }      
     }
-
-    void SetCursor(int x, int y, int color, string text)
+    //закрашиваем координаты
+    void SetCursor(int x, int y, int color)
     {
         COORD position;
         position.X = x;
         position.Y = y;
         SetConsoleCursorPosition(h, position);
         SetConsoleTextAttribute(h, color);
-        cout << text;
     }
-
+    //стены на карте
+    void WallNumberTwo(int x, int y)
+    {
+        COORD position;
+        position.X = x * 3;
+        position.Y = y * 2;
+        SetCursor(x, y, 12);
+        cout << (char)178;
+    }
+    //очистка памяти
     void FreeMemory() {
         for (int y = 0; y < height; y++) {
             delete[] bomber[y];
         }
         delete[] bomber;
     }
-
 };
-
+// main
 int main()
 {
     srand(time(NULL));
-    BombermanGame b(60,20);
-    b.Wall();
-    b.FreeMemory();
-
+    BombermanGame b(60,20);// вызов класса
+    b.Options();//скрываем курсор
+    b.Wall();//основные стны
+    b.WallNumberTwo(6, 5);//стены на карте
+    b.WallNumberTwo(6, 6);//стены на карте
+    b.WallNumberTwo(7, 6);//стены на карте
+    b.WallNumberTwo(8, 6);//стены на карте
+    b.WallNumberTwo(7, 5);//стены на карте
+    b.WallNumberTwo(8, 5);//стены на карте
+    b.WallNumberTwo(0, 0);//стены на карте
 }
